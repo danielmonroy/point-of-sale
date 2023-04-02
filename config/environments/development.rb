61a0a747d25569fcdcab6ac56c65c6457961d73e
Daniel Monroy <ldanielmonroy@gmail.com>
2023-04-01 22:06:14 -0600

    Metronic installed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Sass config
+  config.sass.preferred_syntax = :scss
+  config.sass.syntax = :scss
 end
