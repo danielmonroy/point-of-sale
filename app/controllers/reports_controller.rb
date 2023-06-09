636afd2f2e3ac0a0bb4f8b56a8e67bbb22750e9d
Daniel Monroy <ldanielmonroy@gmail.com>
2023-06-08 22:01:07 -0600

    Fixed calculations for reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,14 +3,14 @@ class ReportsController < ApplicationController
   
   def daily_report
     if params[:date].present?
-      date = params[:date].to_date
+      date = params[:date].to_time + 6.hours
     else
-      date = Time.now.in_time_zone("Mexico City").to_date
+      date = Time.now.in_time_zone("Mexico City").beginning_of_day
     end
 
-    @cash_sales = Payment.cash.for_date(date.beginning_of_day.in_time_zone("Mexico City"), date.end_of_day.in_time_zone("Mexico City")).sum(:total)
-    @card_sales = Payment.card.for_date(date.beginning_of_day.in_time_zone("Mexico City"), date.end_of_day.in_time_zone("Mexico City")).sum(:total)
-    @transfer_sales = Payment.transfer.for_date(date.beginning_of_day.in_time_zone("Mexico City"), date.end_of_day.in_time_zone("Mexico City")).sum(:total)
-    @total_sales = Payment.for_date(date.beginning_of_day.in_time_zone("Mexico City"), date.end_of_day.in_time_zone("Mexico City")).sum(:total)
+    @cash_sales = Payment.cash.for_date(date, date + 24.hours).sum(:total)
+    @card_sales = Payment.card.for_date(date, date + 24.hours).sum(:total)
+    @transfer_sales = Payment.transfer.for_date(date, date + 24.hours).sum(:total)
+    @total_sales = Payment.for_date(date, date + 24.hours).sum(:total)
   end
 end
