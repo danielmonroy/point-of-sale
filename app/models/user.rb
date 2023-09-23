25d7781906121237746c9488247771b3761fe78a
Daniel Monroy <ldanielmonroy@gmail.com>
2023-09-22 22:17:56 -0600

    Added permissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, # :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :user_permissions
+  has_many :permissions, through: :user_permissions
 
   def full_name
     "#{self.first_name} #{self.last_name}"
+  end
+
+  def can(ability)
+    self.permissions.where(ability: ability).any?
   end
 end
